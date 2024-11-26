505056b2164df1c7c486f7520e120ab51b640be9
Aryan Roshan <arynrosh@gmail.com>
2024-11-25 23:48:52 -0800

    Update introduction.py

--- onboard/introduction.py
+++ onboard/introduction.py
@@ -1,6 +1,6 @@
 # INTRODUCE YOURSELF WITH YOUR NAME YEAR/PROGRAM AND FAVOURITE FOOD!
 
-# Albert Zhu - 2nd year Compeng- Oranges not realy
-# Kamin T - 1st year Engineering - mango
+# Albert Zhu - 2nd year Compeng- Oranges
+# Kamin T - 1st year Engineering - Mango
 # Aryan Roshan - 2nd year Computer Engineering - Fried Rice
 # Ryan Li - 4th year Electrical Engineering - Watermelon
